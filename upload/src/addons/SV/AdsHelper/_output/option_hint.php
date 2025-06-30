6bae97e29d0459d2374fd3d6948feac5ef5a84cb
Xon <635541+Xon@users.noreply.github.com>
2025-06-30 17:09:37 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/AdsHelper/_output/option_hint.php
@@ -0,0 +1,22 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $adsHelper_personalizeAds Venatus personalise ads
+ * @property string|null $adsHelper_siteId Venatus site id
+ * @property int|null $svAdEveryXWords Ad every X words
+ * @property bool|null $svAdOnFirstNonThreadmarkPost Ad on first non-threadmark post
+ * @property array|null $svAdsGroupBypass Usergroups to skip ads
+ */
+class Options
+{
+}
